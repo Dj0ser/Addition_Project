05608f3f384380b5366730e82c19c84c32d31a82
Carl Rehnstrand <c.rehnstrand@gmail.com>
2020-02-06 10:50:24 +0100

    Added method for addition and default code.

---
+++ source/src/CarlRehnstrand/AddingNumbers.java
@@ -0,0 +1,18 @@
+package CarlRehnstrand;
+
+import java.util.Scanner;
+
+public class AddingNumbers {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter 2 numbers: ");
+
+    int intNumber1 = input.nextInt();
+    int intNumber2 = input.nextInt();
+
+    System.out.println(intNumber1 + intNumber2);
+
+    }
+}
+
